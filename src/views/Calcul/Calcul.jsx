d25f3e596f9cbdb8819b746593bddb2348acff17
Voxalium <voxalium@gmail.com>
2024-07-15 18:08:34 +0200

    style | reset

--- src/views/Calcul/Calcul.jsx
+++ src/views/Calcul/Calcul.jsx
@@ -2,12 +2,14 @@ import { NavLink } from "react-router-dom";
 export default function Calcul() {
   return (
     <main className="Calcul ">
-      <div className="links column align-center justify-center">
-        <NavLink to="/calcul/mix">Mix</NavLink>
-        <NavLink to="/calcul/addition">Addition</NavLink>
-        <NavLink to="/calcul/substraction">Substraction</NavLink>
-        <NavLink to="/calcul/multiplication">Multiplication</NavLink>
-        <NavLink to="/calcul/division">Division</NavLink>
+      <div className="container column align-center justify-center">
+        <div className="links column justify-center ">
+          <NavLink to="/calcul/mix">Mix</NavLink>
+          <NavLink to="/calcul/addition">Addition</NavLink>
+          <NavLink to="/calcul/substraction">Substraction</NavLink>
+          <NavLink to="/calcul/multiplication">Multiplication</NavLink>
+          <NavLink to="/calcul/division">Division</NavLink>
+        </div>
       </div>
     </main>
   );
--- src/views/Calcul/MakeCalcul.jsx
+++ src/views/Calcul/MakeCalcul.jsx
@@ -1,75 +1,121 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Add, Div, getRandomInt, Mul, Sub } from "../../scripts/calcul";
-export default function MakeCalcul({op, attempts=5}) {
+export default function MakeCalcul({ op }) {
+  const nav = useNavigate();
   const n = getRandomInt();
-  const [score, setScore] = useState(0); 
+  const [score, setScore] = useState(0);
   const [test, setTest] = useState(0);
+  const [rounds, setRounds] = useState(10);
   const [isCorrect, setIsCorrect] = useState(null);
+  const [isRoundStarted, setIsRoundStarted] = useState(false);
 
   let res = 0;
   let calcString = "";
-  switch(op){
+  switch (op) {
     case "+":
-      res = Add(n.a,n.b) ;
+      res = Add(n.a, n.b);
       calcString = `${n.a} + ${n.b}`;
-    break;
+      break;
     case "-":
-      res = Sub(n.a,n.b) ;
+      res = Sub(n.a, n.b);
       calcString = `${n.a} - ${n.b}`;
-    break;
+      break;
     case "*":
-      res = Mul(n.a,n.b) ;
+      res = Mul(n.a, n.b);
       calcString = `${n.a} * ${n.b}`;
-    break;
+      break;
     case "/":
-      res = Number(Div(n.a,n.b).toFixed(2)); ;
+      res = Number(Div(n.a, n.b).toFixed(2));
       calcString = `${n.a} / ${n.b}`;
-    break;
+      break;
     case "Mix":
-      res = Add(n.a,n.b) ;
-    break;
+      res = Add(n.a, n.b);
+      break;
     default:
-      return console.error("Operator not set !");      
+      return console.error("Operator not set !");
   }
 
+  function checkRes() {
+    setTest(test + 1);
 
-  function checkRes(){
-    setTest(test+1);
-
-    const guessInput = Number(document.querySelector("#guess").value); 
-    if(guessInput === res){
-      setIsCorrect(true)
-      setScore(score+1);
-    } else setIsCorrect(false); 
+    const guessInput = Number(document.querySelector("#guess").value);
+    if (guessInput === res) {
+      setIsCorrect(true);
+      setScore(score + 1);
+    } else setIsCorrect(false);
 
     document.querySelector("#guess").value = "";
-
   }
 
-  if(test !==attempts) {
+  if (!isRoundStarted) {
+    return (
+      <main className="MakeCalcul">
+        <div className="container column full-center">
+          <h1>Set Rounds</h1>
+          <form action="">
+            <input type="text" name="rounds" id="rounds" />
+            <input
+              type="submit"
+              id="submit"
+              value="Validate"
+              onClick={(e) => {
+                e.preventDefault();
+                setRounds(Number(document.querySelector("#rounds").value));
+                setIsRoundStarted(true);
+              }}
+            />
+          </form>
+        </div>
+      </main>
+    );
+  } else {
+    if (test !== rounds) {
+      return (
+        <main className="MakeCalcul">
+          <h1>Score:{score}</h1>
+          <h2>
+            Test:{test}/{rounds}
+          </h2>
+          <div className="container column full-center">
+            <p>{calcString}</p>
+            <form action="" className="guess-form">
+              <label htmlFor="guess">Guess : </label>
+              <input type="text" name="guess" id="guess" />
+              <input
+                type="submit"
+                value="Guess"
+                onClick={(e) => {
+                  e.preventDefault();
+                  checkRes();
+                }}
+              />
+              {isCorrect === false && <p>Wrong</p>}
+            </form>
+          </div>
+        </main>
+      );
+    } else
+      return (
+        <main className="MakeCalcul">
+          <div className="container column full-center">
+            <h1>
+              Score:{score}/{rounds}
+            </h1>
 
-    return(
-    <main className="MakeCalcul">
-        
-      <h1>Score:{score}</h1>
-      <h2>Test:{test}/{attempts}</h2>
-      <div className="container column full-center">
-        <p>{calcString}</p>
-        <form action="" className="guess-form">
-          <label htmlFor="guess">Guess : </label>
-          <input type="text" name="guess" id="guess"/>
-          <input type="submit" value="Guess" onClick={(e)=>{
-            e.preventDefault();
-            checkRes()
-          }} />
-          {isCorrect === false && <p>Wrong</p>}
-               </form>
-      </div>
-    </main>
-  );
-  } else return (
-  <main className="MakeCalcul">
-    <h1>Score:{score}/{attempts}</h1>
-  </main>
-  )    
+            <form action="">
+              <input
+                type="submit"
+                value="Return"
+                id="reset"
+                onClick={(e) => {
+                  e.preventDefault();
+                  nav("/calcul");
+                }}
+              />
+            </form>
+          </div>
+        </main>
+      );
+  }
 }
